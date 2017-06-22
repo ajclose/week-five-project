107061841695311372463b1a4afaf6741211c506
Alex Close <ajclose@gmail.com>
2017-06-22 16:00:41 -0500

    Generate blank spaces for word to be guessed

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+let sess;
+
+function generateGuess(wordLength) {
+  let guess = []
+  for (var i = 0; i < wordLength; i++) {
+    guess[i] = "_ "
+  }
+  return guess.join('')
+}
 
 app.set('view engine', 'mustache')
 app.engine('mustache', mustache())
@@ -21,9 +30,12 @@ app.listen(3000, function() {
 })
 
 app.get('/', function(req, res) {
+  sess = req.session
   const randomIndex = Math.floor(Math.random()*(words.length-1))
   const word = words[randomIndex]
+  const guess = generateGuess(word.length)
   res.render('index', {
-    word: word
+    word: word,
+    guess: guess
   })
 })
